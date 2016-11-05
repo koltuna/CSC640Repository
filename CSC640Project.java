a59097af18daa6d567254a6a0415f93b6ff08458
koltuna <akoltun@pio.carrollu.edu>
2016-11-05 09:25:10 -0500

    Add files via upload

---
+++ CSC640Project.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package csc640project;
+//This is the source code for the main class
+/**
+ *
+ * @author Alex
+ */
+public class CSC640Project {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        MainGUIInterface.main(null);
+    }
+    
+}
